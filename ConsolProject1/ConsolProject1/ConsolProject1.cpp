606bb1975cc50c955e5399ecde69dc8f5a6fe80d
rhdddv <100360508+Yarik207@users.noreply.github.com>
2025-07-29 15:13:18 +0400

    1.2
    Решена проблема с регистрами

--- ConsolProject1/ConsolProject1/ConsolProject1.cpp
+++ ConsolProject1/ConsolProject1/ConsolProject1.cpp
@@ -1,22 +1,39 @@
 ﻿#include <iostream>
 #include <string>
+#include <algorithm>
+
 
 using namespace std;
+
+void botAnswer(string msg) {
+    cout << "[BOT]: " << msg << endl; 
+}
+
+// приводит весь текст к нижнему регистру 
+string toLower(string text) {
+    transform(text.begin(), text.end(), text.begin(), ::toLower); //приводим текст к нижнему регистру 
+    return text;
+}
 
 int main()
 {
     string question;
 
     getline(cin, question); //Получает ввод от пользователя в переменную qustion
+    question = toLower(question); 
 
     cout << "Question: " << question << endl; // Выводит текст от пользователя на экран 
 
     if (question == "Hello") {
-        cout << "Hi, how are you?\n";
+        botAnswer ("Hi, how are you?\n") ;
     }
 
     if (question == "Hi, how are you") {
-        cout << "I'm fine, thanks!\n";
+        botAnswer ("I'm fine, thanks!\n");
+    }
+
+    if (question == "What is your name?") {
+        botAnswer("My name is SkillChat\n");
     }
 
 
