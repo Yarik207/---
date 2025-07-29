b63623ddab43d249c8da34a134970180415917a5
rhdddv <100360508+Yarik207@users.noreply.github.com>
2025-07-29 13:45:35 +0400

    1.1
    Способен отвечать на 2 запроса пользователя

---
+++ ConsolProject1/ConsolProject1/ConsolProject1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string question;
+
+    getline(cin, question); //Получает ввод от пользователя в переменную qustion
+
+    cout << "Question: " << question << endl; // Выводит текст от пользователя на экран 
+
+    if (question == "Hello") {
+        cout << "Hi, how are you?\n";
+    }
+
+    if (question == "Hi, how are you") {
+        cout << "I'm fine, thanks!\n";
+    }
+
+
+
+}
